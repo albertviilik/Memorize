bfc90c91df07108116fe6b986612bacbff52ab18
Albert Viilik <42554671+albertviilik@users.noreply.github.com>
2021-10-15 16:42:29 +0100

    Lecture 1 & 2

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,9 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
+    // Add 24 things
+    var emojis = ["🚗", "⛴", "✈️", "🚜", "🛴", "🚲", "🏍", "🛺", "🚠", "🚟", "🚄", "🚅", "🚈", "🚂", "🚇", "🚊", "🚉", "🚤", "🛬", "🚀", "🛸", "🚁", "🛶", "⛵️"]
+    @State var emojiCount = 20
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji, faceUp: true)
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+            .padding(.horizontal)
+            .foregroundColor(.red)
+            Spacer()
+            HStack {
+                remove
+                Spacer()
+                add
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
+        }
+    }
+    
+    var remove: some View {
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+        }
+    }
+    
+    var add: some View {
+        Button {
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        } label: {
+            Image(systemName: "plus.circle")
+        }
+    }
+}
+
+struct CardView: View {
+    var content: String
+    
+    @State var faceUp: Bool
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if faceUp {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 3)
+                Text(content)
+                    .font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }.onTapGesture {
+            self.faceUp = !self.faceUp
+        }
     }
 }
 
